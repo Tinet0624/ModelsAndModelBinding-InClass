e3641a3539b511e8c77201f81274b9b2d8411203
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-07-19 16:50:40 -0700

    Added Post option to Student controller

--- ModelsAndModelBinding/Controllers/StudentController.cs
+++ ModelsAndModelBinding/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelsAndModelBinding.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,25 @@ namespace ModelsAndModelBinding.Controllers
 {
     public class StudentController : Controller
     {
+        //user goes to page
+        [HttpGet] //Atribute for methods this one is default *get shows it in the URL
         public IActionResult Add()
         {
+            return View();
+        }
+
+        //sending data to a server
+        [HttpPost] // After user posts form data
+        public IActionResult Add(IFormCollection form)
+        {
+            Student stu = new Student() 
+            { 
+                FullName = form["full-name"],
+                DateOfBirth = Convert.ToDateTime(form["dob"]),
+                Email = form["email"],
+                PhoneNumber = form["phone-number"]
+            };
+            
             return View();
         }
     }
